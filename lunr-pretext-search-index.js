f8be9d2fffa83d6934077bf8145c30cfc6b980cd
William Haynes <whaynes@maritime.edu>
2024-12-15 10:30:06 -0500

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,221 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "TSPS-principle-dimensions",
+  "level": "1",
+  "url": "TSPS-principle-dimensions.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Principle Dimensions",
+  "body": " Principle Dimensions   Principle Dimensions    Length, overall  160 m  (525 ft)    Beam  27 m  (89 ft)    Depth, to main deck  16.8 m  (55 ft)    Draft, load line  7.35 m  (24 ft)    Masthead Height, above keel  44 m  (144 ft)    Total installed Power  16,800 kW  (22,500 hp)    Design Speed  18 knots  (20.7 mph)      Capacities    Cadet Berths  600    Officers and Crew Berths (Single Occupancy)  100    Additional Berths (Double Occupancy)  60    RO-RO Cargo Space  990 m    Deck Container Capacity  60 TEU    Fuel Marine Gas Oil  2169 m    Fresh Water  1234 m    Salt Water Ballast  3567 m    Fresh Water Ballast  1275 m    Gray Water  655 m    Black Water  166 m     "
+},
+{
+  "id": "principle_dimensions",
+  "level": "2",
+  "url": "TSPS-principle-dimensions.html#principle_dimensions",
+  "type": "Table",
+  "number": "1.1.1",
+  "title": "Principle Dimensions",
+  "body": " Principle Dimensions    Length, overall  160 m  (525 ft)    Beam  27 m  (89 ft)    Depth, to main deck  16.8 m  (55 ft)    Draft, load line  7.35 m  (24 ft)    Masthead Height, above keel  44 m  (144 ft)    Total installed Power  16,800 kW  (22,500 hp)    Design Speed  18 knots  (20.7 mph)    "
+},
+{
+  "id": "nsmv_capacities",
+  "level": "2",
+  "url": "TSPS-principle-dimensions.html#nsmv_capacities",
+  "type": "Table",
+  "number": "1.1.2",
+  "title": "Capacities",
+  "body": " Capacities    Cadet Berths  600    Officers and Crew Berths (Single Occupancy)  100    Additional Berths (Double Occupancy)  60    RO-RO Cargo Space  990 m    Deck Container Capacity  60 TEU    Fuel Marine Gas Oil  2169 m    Fresh Water  1234 m    Salt Water Ballast  3567 m    Fresh Water Ballast  1275 m    Gray Water  655 m    Black Water  166 m    "
+},
+{
+  "id": "TSPS-deck-plans",
+  "level": "1",
+  "url": "TSPS-deck-plans.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Deck Plans",
+  "body": " Deck Plans   Patriot State Waterline Profile    MARAD      Patriot State Outboard Profile    MARAD      Patriot State Inboard Profile Port    MARAD      Patriot State Inboard Profile Starboard    MARAD      Patriot State 05 Level    MARAD      Patriot State 04 Level    MARAD      Patriot State O3 Level    MARAD      Patriot State 02 Level    MARAD      Patriot State 01 Level    MARAD      Patriot State Main Deck    MARAD      Patriot State 2nd Deck    MARAD      Patriot State 3rd Deck    MARAD      Patriot State 4th Deck    MARAD      Patriot State Tank Tops    MARAD      Patriot State Inner Bottom    MARAD     "
+},
+{
+  "id": "fig_waterline_profile",
+  "level": "2",
+  "url": "TSPS-deck-plans.html#fig_waterline_profile",
+  "type": "Figure",
+  "number": "1.2.1",
+  "title": "",
+  "body": " Patriot State Waterline Profile    MARAD    "
+},
+{
+  "id": "fig_outboard_profile",
+  "level": "2",
+  "url": "TSPS-deck-plans.html#fig_outboard_profile",
+  "type": "Figure",
+  "number": "1.2.2",
+  "title": "",
+  "body": " Patriot State Outboard Profile    MARAD    "
+},
+{
+  "id": "fig_inboard_port",
+  "level": "2",
+  "url": "TSPS-deck-plans.html#fig_inboard_port",
+  "type": "Figure",
+  "number": "1.2.3",
+  "title": "",
+  "body": " Patriot State Inboard Profile Port    MARAD    "
+},
+{
+  "id": "fig_inboard_stbd",
+  "level": "2",
+  "url": "TSPS-deck-plans.html#fig_inboard_stbd",
+  "type": "Figure",
+  "number": "1.2.4",
+  "title": "",
+  "body": " Patriot State Inboard Profile Starboard    MARAD    "
+},
+{
+  "id": "fig_05_level",
+  "level": "2",
+  "url": "TSPS-deck-plans.html#fig_05_level",
+  "type": "Figure",
+  "number": "1.2.5",
+  "title": "",
+  "body": " Patriot State 05 Level    MARAD    "
+},
+{
+  "id": "fig_04_level",
+  "level": "2",
+  "url": "TSPS-deck-plans.html#fig_04_level",
+  "type": "Figure",
+  "number": "1.2.6",
+  "title": "",
+  "body": " Patriot State 04 Level    MARAD    "
+},
+{
+  "id": "fig_03_level",
+  "level": "2",
+  "url": "TSPS-deck-plans.html#fig_03_level",
+  "type": "Figure",
+  "number": "1.2.7",
+  "title": "",
+  "body": " Patriot State O3 Level    MARAD    "
+},
+{
+  "id": "fig_02_level",
+  "level": "2",
+  "url": "TSPS-deck-plans.html#fig_02_level",
+  "type": "Figure",
+  "number": "1.2.8",
+  "title": "",
+  "body": " Patriot State 02 Level    MARAD    "
+},
+{
+  "id": "fig_01_level",
+  "level": "2",
+  "url": "TSPS-deck-plans.html#fig_01_level",
+  "type": "Figure",
+  "number": "1.2.9",
+  "title": "",
+  "body": " Patriot State 01 Level    MARAD    "
+},
+{
+  "id": "fig_main_deck",
+  "level": "2",
+  "url": "TSPS-deck-plans.html#fig_main_deck",
+  "type": "Figure",
+  "number": "1.2.10",
+  "title": "",
+  "body": " Patriot State Main Deck    MARAD    "
+},
+{
+  "id": "fig_2nd_deck",
+  "level": "2",
+  "url": "TSPS-deck-plans.html#fig_2nd_deck",
+  "type": "Figure",
+  "number": "1.2.11",
+  "title": "",
+  "body": " Patriot State 2nd Deck    MARAD    "
+},
+{
+  "id": "fig_3rd_deck",
+  "level": "2",
+  "url": "TSPS-deck-plans.html#fig_3rd_deck",
+  "type": "Figure",
+  "number": "1.2.12",
+  "title": "",
+  "body": " Patriot State 3rd Deck    MARAD    "
+},
+{
+  "id": "fig_4th_deck",
+  "level": "2",
+  "url": "TSPS-deck-plans.html#fig_4th_deck",
+  "type": "Figure",
+  "number": "1.2.13",
+  "title": "",
+  "body": " Patriot State 4th Deck    MARAD    "
+},
+{
+  "id": "fig_tank_top",
+  "level": "2",
+  "url": "TSPS-deck-plans.html#fig_tank_top",
+  "type": "Figure",
+  "number": "1.2.14",
+  "title": "",
+  "body": " Patriot State Tank Tops    MARAD    "
+},
+{
+  "id": "fig_inner_bottom",
+  "level": "2",
+  "url": "TSPS-deck-plans.html#fig_inner_bottom",
+  "type": "Figure",
+  "number": "1.2.15",
+  "title": "",
+  "body": " Patriot State Inner Bottom    MARAD    "
+},
+{
+  "id": "TSPS-ship-s-history",
+  "level": "1",
+  "url": "TSPS-ship-s-history.html",
+  "type": "Section",
+  "number": "1.3",
+  "title": "Ship’s History",
+  "body": " Ship's History  Construction History: design started, keel laid, ship launched, ship delivered, first cruise,$300 million cost, Philly shipyard, TOTE services.  "
+},
+{
+  "id": "main-propulsion-main-engines",
+  "level": "1",
+  "url": "main-propulsion-main-engines.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "Main Engines",
+  "body": " Main Engines  Turbocharged, 4-stroke, single acting, trunk piston, non-reversible, direct fuel injection   Main Engine and Generator    own work weh         Manufacturer  GE Wabtec    Model  16 V250MDC    Maximum Continuous Power  4200 kW (5632 hp)    Rated Speed  900 rpm    Arrangement and number of Cylinders  V-16    Bore  250 mm (9.8 in)    Stroke  320 mm (12.6 in)     "
+},
+{
+  "id": "main_engine_generator",
+  "level": "2",
+  "url": "main-propulsion-main-engines.html#main_engine_generator",
+  "type": "Figure",
+  "number": "2.1.1",
+  "title": "",
+  "body": " Main Engine and Generator    own work weh    "
+},
+{
+  "id": "main_engine_info",
+  "level": "2",
+  "url": "main-propulsion-main-engines.html#main_engine_info",
+  "type": "Table",
+  "number": "2.1.2",
+  "title": "",
+  "body": "    Manufacturer  GE Wabtec    Model  16 V250MDC    Maximum Continuous Power  4200 kW (5632 hp)    Rated Speed  900 rpm    Arrangement and number of Cylinders  V-16    Bore  250 mm (9.8 in)    Stroke  320 mm (12.6 in)    "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
